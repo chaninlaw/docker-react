a133e949f44395fd94619bbc85ea2473f9395491
Chanin Law <chaninlawlert@gmail.com>
2023-09-16 12:37:31 +0700

    test by ninja

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <h2>Hi there! by Ninja</h2>
+        <h2>Test! by Ninja</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
