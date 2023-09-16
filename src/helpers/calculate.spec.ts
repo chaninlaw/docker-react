d4cb61d537459286c6a8a627675fe878ddc0068d
Chanin Law <chaninlawlert@gmail.com>
2023-09-16 09:55:28 +0700

    initial commit'

---
+++ src/helpers/calculate.spec.ts
@@ -0,0 +1,13 @@
+import { add } from './ calculate'
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3)
+  })
+})
+
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(1, 2)).toBe(3)
+  })
+})
